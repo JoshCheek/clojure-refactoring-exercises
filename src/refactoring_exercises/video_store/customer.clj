4e3d7d9f40fcf23e63a76f0a275721e94e93a541
Josh Cheek <josh.cheek@gmail.com>
2017-06-02 10:53:05 -0500

    Video store customer: refactor away the loop/recur

--- src/refactoring_exercises/video_store/customer.clj
+++ src/refactoring_exercises/video_store/customer.clj
@@ -7,33 +7,41 @@
 (defn add-rental [customer rental]
   (update-in customer [:rentals] (fnil conj []) rental))
 
+(defn amount-for [rental]
+  (condp = (:price-code (:movie rental))
+    movie/REGULAR (+ 2 (if (< 2 (:days rental))
+                         (* 1.5 (- (:days rental) 2))
+                         0))
+    movie/NEW_RELEASE (* 3 (:days rental))
+    movie/CHILDRENS (+ 1.5 (if (< 3 (:days rental))
+                             (* 1.5 (- (:days rental) 3))
+                             0))))
+
+(defn frequent-points-for [rental]
+  (if (and (< 1 (:days rental))
+           (= movie/NEW_RELEASE
+              (:price-code (:movie rental))))
+    2
+    1))
+
+(defn format-line-item-for [rental amount]
+  (str "\t" (:title (:movie rental)) "\t" (double amount) "\n"))
+
+(defn summary-for [customer]
+  (reduce (fn [[line-items amount points]
+               rental]
+            (let [this-amount (amount-for rental)]
+              [(str line-items (format-line-item-for rental this-amount))
+               (+ amount this-amount)
+               (+ points (frequent-points-for rental))
+               ]))
+          ["" 0.0 0]
+          (:rentals customer)))
+
 (defn statement-body [customer]
-  (loop [text ""
-         rentals (:rentals customer)
-         total-amount 0.0
-         frequent-renter-points 0]
-    (if-let [rental (first rentals)]
-      (let [this-amount (condp = (:price-code (:movie rental))
-                          movie/REGULAR (+ 2 (if (< 2 (:days rental))
-                                               (* 1.5 (- (:days rental) 2))
-                                               0))
-                          movie/NEW_RELEASE (* 3 (:days rental))
-                          movie/CHILDRENS (+ 1.5 (if (< 3 (:days rental))
-                                                   (* 1.5 (- (:days rental) 3))
-                                                   0)))
-            these-renter-points (if (and (< 1 (:days rental))
-                                         (= movie/NEW_RELEASE
-                                            (:price-code (:movie rental))))
-                                  2
-                                  1)
-            line-item (str "\t" (:title (:movie rental)) "\t" (double this-amount) "\n")]
-        (recur (str text line-item)
-               (rest rentals)
-               (+ total-amount this-amount)
-               (+ frequent-renter-points these-renter-points)))
-      {:body-text text
-       :total-amount total-amount
-       :frequent-renter-points frequent-renter-points})))
+  (zipmap
+    [:body-text :total-amount :frequent-renter-points]
+    (summary-for customer)))
 
 (defn statement-header [customer]
   (format "Rental Record for %s\n" (:name customer)))
